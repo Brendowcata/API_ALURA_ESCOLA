3d19a8b9eeb05d1d230a39e1262a12dd88621e6a
Brendow Catarina Dellarmelin <brendowcata1@gmail.com>
2022-03-24 00:10:31 -0300

    Models, Admin and Serializer

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=30, db_column="NOME")
+    rg = models.CharField(max_length=9, db_column="RG")
+    cpf = models.CharField(max_length=11, db_column="CPF")
+    data_nascimento = models.DateField(db_column="DATA_NASCIMENTO")
+
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado')
+    )
+
+    codigo_curso = models.CharField(max_length=10, db_column="CODIGO_CURSO")
+    descricao = models.CharField(max_length=100, db_column="DESCRICAO")
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, default='B', db_column="NIVEL")
+
+    def __str__(self):
+        return self.descricao
